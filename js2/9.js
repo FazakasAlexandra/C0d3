e1b101a38ea1273ad8284d8e6b2007cef0e2810e
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2020-07-28 12:17:40 +0300

    solves problem 9

--- js2/9.js
+++ js2/9.js
@@ -5,8 +5,21 @@
  */
 
 const solution = () => {
-  Array.prototype.cReduce = function () {
-    return 0
+  Array.prototype.cReduce = function (cb, acc, i = 0, result) {
+    if (i === this.length) {
+      return result
+    }
+
+    let condition = acc === undefined && acc !== "" && acc !== 0 && acc !== [] && acc !== {}
+    if (condition) {
+      result = cb(this[i], this[i + 1], i, this)
+      return this.cReduce(cb, result, i + 2, result)
+    }
+
+    result = cb(acc, this[i], i, this)
+    acc = result
+
+    return this.cReduce(cb, acc, i + 1, result)
   }
 }
 
