9533d624dd14c881ab5fa92847b5323f83ce529d
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2020-07-30 12:24:47 +0300

    preflight js3 done

--- js3/preflight.js
+++ js3/preflight.js
@@ -16,4 +16,46 @@ function mergeArrays(one, two, merged = [], i = 0, j = 0){
 
 allFuns.mergeArrays = mergeArrays 
 
+function firstLongerThan(arr, nr, i = 0) {
+    if(i === arr.length){
+        return 
+    }
+
+    if(arr[i].length > nr){
+        return arr[i]
+    }
+
+    return firstLongerThan(arr, nr, i + 1)
+
+}
+allFuns.firstLongerThan = firstLongerThan
+
+function getReturnValues(arr, values = [], i = 0){
+    if(i === arr.length){
+        return values
+    }
+    let fn = arr[i]
+    values.push(fn())
+    return getReturnValues(arr, values, i + 1)
+}
+allFuns.getReturnValues = getReturnValues
+
+function getColArr(nr, colArr = []){
+    if(colArr.length < nr ){
+        colArr.push(0)
+        return getColArr(nr, colArr)
+    }
+    return colArr
+}
+function zeroSquare(nr, rowArr = []){
+    if(rowArr.length < nr){
+        rowArr[rowArr.length] = getColArr(nr)
+        return zeroSquare(nr, rowArr)
+    }
+
+    return rowArr
+}
+allFuns.zeroSquare = zeroSquare
+
+
 module.exports = allFuns
--- js3/preflight.test.js
+++ js3/preflight.test.js
@@ -16,3 +16,69 @@ describe('mergeArrays function', () => {
         expect(result).toEqual(["Pikachu", "Raichu"])
     })
 })
+
+describe('firstLongerThan function', () => {
+    it('should find a string in the middle of an array', () => {
+				const arr = ["Ekans", "Arbok", "Pikachu", "Raichu"]
+        const result = fn.firstLongerThan(arr, 5)
+        expect(result).toEqual("Pikachu")
+    })
+    it('should find a string at the end of an array', () => {
+				const arr = ["Caterpie", "Metapod", "Butterfree"]
+        const result = fn.firstLongerThan(arr, 9)
+        expect(result).toEqual("Butterfree")
+    })
+    it('should find the first string longer than 0', () => {
+        const result = fn.firstLongerThan(["a", "b", "c"], 0)
+        expect(result).toEqual("a")
+    })
+    it('should return undefined', () => {
+      const result = fn.firstLongerThan([], 5)
+      expect(result).toEqual(undefined)
+    })
+})
+
+describe('getReturnValues function', () => {
+    const fn1 = () => { return 25 }
+    const fn2 = () => { return true }
+    const fn3 = () => { return "Pikachu" }
+    const fn4 = () => { "I'm function four" }
+
+    it('should get 3 return values of various types', () => {
+        const result = fn.getReturnValues([fn1, fn2, fn3])
+        expect(result).toEqual([25, true, "Pikachu"])
+    })
+    it('should return an empty array if no functions', () => {
+        const result = fn.getReturnValues([])
+        expect(result).toEqual([])
+    })
+    it('should return undefined for functions with no return value', () => {
+        const result = fn.getReturnValues([fn4])
+        expect(result).toEqual([undefined])
+    })
+})
+
+describe('zeroSquare function', () => {
+    it('should create a 1x1 array of zeroes', () => {
+        const square1 = [[0]]
+        expect(fn.zeroSquare(1)).toEqual(square1)
+    })
+    it('should create a 1x1 array of zeroes', () => {
+        const square2 = [
+            [0,0],
+            [0,0]
+        ]
+        expect(fn.zeroSquare(2)).toEqual(square2)
+    })
+    it('should create a 1x1 array of zeroes', () => {
+        const square3 = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+          ]
+        expect(fn.zeroSquare(3)).toEqual(square3)
+    })
+    it('should return an empty array for 0 value', () => {
+        expect(fn.zeroSquare(0)).toEqual([])
+    })
+})
