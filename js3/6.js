258fe75477c1d46b65d42edc4bdf9de2cb4181a5
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2020-08-21 14:08:37 +0300

    solves problem 6

--- js3/6.js
+++ js3/6.js
@@ -4,8 +4,16 @@
  * @returns {array}
 */
 
-const solution = (arr) => {
-  return []
+const solution = (arr, duplicates = [], obj = []) => {
+  arr.forEach(e => {
+    if(obj[e] && obj[e].occurences === 1){
+      duplicates.push(e)
+      obj[e].occurences += 1
+    } else {
+      obj[e] = {occurences : 1}
+    }
+  });
+  return duplicates
 }
 
 module.exports = {
