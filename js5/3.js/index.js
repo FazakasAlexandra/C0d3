92187c49a49693a8e351ea9c9cee51975370e18f
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-03-14 10:19:48 +0200

    changes js5/3.js

--- js5/3.js/index.js
+++ js5/3.js/index.js
@@ -1,30 +1,43 @@
 const jimp = require('jimp');
 const express = require('express');
 const app = express();
+const cache = {}
 
 app.get('/memegen/api/:text', (req, res) => {
     src = req.query.src || 'https://placeimg.com/640/480/any'
     blurScale = parseInt(req.query.blur) || 0
-    black = req.query.black.toLowerCase() || false
+    black = req.query.black || 'false'
+    font = black.toLowerCase() === 'true' ? jimp.FONT_SANS_32_BLACK : jimp.FONT_SANS_32_WHITE
+    text = req.params.text
 
-    console.log(black)
-    jimp.loadFont(jimp.FONT_SANS_32_WHITE)
-        .then(font => {
-            jimp.read(src, (err, img) => {
-                if (err) console.log(err)
+    jimp.loadFont(font).then(font => {
 
-                img.print(font, 0, 0, req.params.text)
+        if (cache[src]) {
+            manipulateImg(cache[src].clone(), font, text, blurScale, res)
+            return
+        }
+        
+        jimp.read(src, (err, img) => {
+            if (err) console.log(err)
 
-                if (black === 'true') img.greyscale((err, imgBlack) => img = imgBlack)
+            cache.count = (cache.count || 0) + 1
+            if(cache.count <= 10) cache[src] = img
 
-                if (blurScale) {
-                    img.blur(blurScale, (err, imgBlur) => sendImg(imgBlur, res))
-                } else {
-                    sendImg(img, res)
-                }
-            })
+            manipulateImg(img.clone(), font, text, blurScale,res)
         })
+    })
 })
+
+function manipulateImg(img, font, text, blurScale, res) {
+    img.print(font, 0, 0, text)
+
+    if (blurScale) {
+        img.blur(blurScale, (err, imgBlur) => sendImg(imgBlur, res))
+        return
+    }
+
+    sendImg(img, res)
+}
 
 function sendImg(img, res) {
     img.getBufferAsync(jimp.MIME_JPEG).then((buffer) => {
